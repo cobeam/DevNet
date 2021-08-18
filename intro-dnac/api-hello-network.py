d824bb6ad7d21bcae03485b571e97fc2b6b61df9
Colby Beam <83893074+cobeam@users.noreply.github.com>
2021-08-18 00:26:32 -0400

    hello networking api calls

--- intro-dnac/api-hello-network.py
+++ intro-dnac/api-hello-network.py
@@ -1,32 +1,32 @@
-import requests
 import json
+import requests
 
+controller_ip = "sandboxdnac.cisco.com"
+DNAC_PORT = 443
+DNAC_USER = "devnetuser"
+DNAC_PASS = "Cisco123!"
 
-def get_auth_token(controller_ip, DNAC_PORT, DNAC_USER, DNAC_PASSWORD):
+def get_auth_token():
     """ Authenticates with controller and returns a token to be used in subsequent API invocations
     """
-    HTTPBasicAuth = (DNAC_USER, DNAC_PASSWORD)
     login_url = "https://{0}:{1}/dna/system/api/v1/auth/token".format(controller_ip, DNAC_PORT)
-    result = requests.post(url=login_url, auth=HTTPBasicAuth, verify=False)
+    result = requests.post(url=login_url, auth=(DNAC_USER, DNAC_PASS), verify=False)
     result.raise_for_status()
-
     token = result.json()["Token"]
     return {
         "controller_ip": controller_ip,
         "token": token
     }
 
-def create_url(path, controller_ip, DNAC_PORT):
+def create_url(path):
     """ Helper function to create a DNAC API endpoint URL
     """
     return "https://%s:%s/api/v1/%s" % (controller_ip, DNAC_PORT, path)
 
+def get_url(url):
 
-def get_url(url, controller_ip, DNAC_PORT, DNAC_USER, DNAC_Password):
-
-    url = create_url(url, controller_ip, DNAC_PORT)
-    print(url)
-    token = get_auth_token(controller_ip, DNAC_PORT, DNAC_USER, DNAC_Password)
+    url = create_url(path=url)
+    token = get_auth_token()
     headers = {'X-auth-token' : token['token']}
     try:
         response = requests.get(url, headers=headers, verify=False)
@@ -37,12 +37,25 @@ def get_url(url, controller_ip, DNAC_PORT, DNAC_USER, DNAC_Password):
     return response.json()
 
 def list_network_devices():
-    return get_url("network-device", "sandboxdnac.cisco.com", 443, "devnetuser", "Cisco123!")
+    return get_url("network-device")
 
+def ip_to_id(ip):
+    return get_url("network-device/ip-address/%s" % ip)['response']['id']
+
+def get_modules(id):
+   return get_url("network-device/module?deviceId=%s" % id)
+
+def print_info(modules):
+    print("{0:30}{1:15}{2:25}{3:5}".format("Module Name","Serial Number","Part Number","Is Field Replaceable?"))
+    for module in modules['response']:
+        print("{moduleName:30}{serialNumber:15}{partNumber:25}{moduleType:5}".format(moduleName=module['name'],
+                                                           serialNumber=module['serialNumber'],
+                                                           partNumber=module['partNumber'],
+                                                           moduleType=module['isFieldReplaceable']))
 
 if __name__ == "__main__":
     response = list_network_devices()
-    print("\n{0:42}{1:17}{2:12}{3:18}{4:12}{5:16}{6:15}".
+    print("{0:42}{1:17}{2:12}{3:18}{4:12}{5:16}{6:15}".
         format("hostname","mgmt IP","serial",
         "platformId","SW Version","role","Uptime"))
 
@@ -55,3 +68,6 @@ if __name__ == "__main__":
                     device['platformId'],
                     device['softwareVersion'],
                     device['role'],uptime))
+ip_input = input("\nWhat IP address are you interested in? ")
+module = print_info(get_modules(ip_to_id(ip_input)))
+
